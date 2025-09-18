e066c4b05a17958f56817313f500f4b5ae46c7ca
Zackf0s <isaacarrua13@gmail.com>
2025-09-17 21:47:22 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# FELIZ-CUMPLEA-OS
+The source code comes from https://github.com/AJLoveChina/birthday with some of my changes.
+
+Domestic users visit: https://sg.chenjianhui.site/girlfriend-gift-collection/birth-want-to-say/
+Github Pages: https://calebman.github.io/girlfriend-gift-collection/birth-want-to-say/
+
+How to modify it to your own content
+1. Modify configuration content
+
+Modify the config.js file in the birth-want-to-say directory to achieve the desired effect
+
+texts: represents the playback content
+imgs: images corresponding to the content, one-to-one correspondence through subscripts
+2. Deploy static resources
+
+Just deploy birth-want-to-say directly to the static file server
---
+++ config.js
@@ -0,0 +1,30 @@
+
+var config = {
+    texts: [
+        "No puedo esperar a poder llenarte de besos y abrazoz",      
+        "De tener miles de fotos juntos",  
+        "de poder dormir juntos y despertar juntos",
+        "de jugar y ver series juntos",
+        "o de simplemente estar acostados",
+        "se que vamos a llegar muy lejos señorita",
+        "y tener esa paz que ambos tanto deseamos",
+        "¡juntos!",
+
+        "vos tranquila que tal vez hoy no te pueda dar nada",
+        "pero en un futuro no nos faltara nada amor",
+        "te amo muchisimo. ¡MUAACK! :3",
+    ],
+    
+    desc: {
+        turn_on: "Hoy es 19 de Septiembre",
+        play: "Un año mas en tu vida y un año mas que estas en mi vida",
+        bannar_coming: "Gracias por todo lo que me das sin darte cuenta",
+        balloons_flying: "¡sos hermosa!",
+        cake_fadein: "apenas 19 años la criatura y ya tenes canas",
+        light_candle: "Esto es un poco conmovedor, dado que nunca pense que llegariamos tan lejos",
+        wish_message: "pero me alegro de haber llegado a este momento",
+        story: "donde puedo decir que sos mi hermosa novia!!!"
+    },
+
+    loveText: 'Te amo'
+};
